51d9ffae400f632ca603f44910f8296eaa84dcdd
Corey DeRossett <cderossett3@gmail.com>
2024-02-02 12:56:55 -0500

    made comments

---
+++ src/EvenOdd.java
@@ -0,0 +1,7 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+
+        //Declaring and initializing variables
+        //Using an if statement to determine even or odd
+    }
+}
