38bad38b93d54a53cc12e233251fd79f830817b5
Corey DeRossett <cderossett3@gmail.com>
2024-02-02 13:12:18 -0500

    Declared variable, created an if else statement to determine if variable is even or odd

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -2,6 +2,12 @@ public class EvenOdd {
     public static void main(String[] args) {
 
         //Declaring and initializing variables
-        //Using an if statement to determine even or odd
+        int numToExamine = 5;
+        //Using an if else statement to determine even or odd
+        if(numToExamine % 2 == 0) {
+            System.out.println("The number " + numToExamine + " is even");
+        } else {
+            System.out.println("The number " + numToExamine + " is odd");
+        }
     }
 }
